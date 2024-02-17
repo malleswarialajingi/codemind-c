dc2b740a43d4bd431354ba09d940278eae92dd9d
malleswarialajingi <147472888+malleswarialajingi@users.noreply.github.com>
2024-02-17 17:17:39 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[1000];     scanf("%s",str);     int len = strlen(str);     int sum = 0;     for(int i = 0; i < len ; i++){         if(str[i] >= '1' && str[i] <= '9'){             sum = sum + ( str[i] - '0');         }     }     printf("%d",sum); }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[1000];
+    scanf("%s",str);
+    int len = strlen(str);
+    int sum = 0;
+    for(int i = 0; i < len ; i++){
+        if(str[i] >= '1' && str[i] <= '9'){
+            sum = sum + ( str[i] - '0');
+        }
+    }
+    printf("%d",sum);
+}
